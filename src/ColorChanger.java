5689432834df8d5a5fb509b93b668d6c4e299147
EliasEgel <115075790+EliasEgel@users.noreply.github.com>
2022-12-16 10:53:23 +0100

    random color function

--- src/ColorChanger.java
+++ src/ColorChanger.java
@@ -1,9 +1,16 @@
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ColorChanger {
-    private List<Color> colorList = Arrays.asList(Color.GREEN,Color.BLUE, Color.MAGENTA, Color.CYAN, Color.ORANGE, Color.PINK, Color.YELLOW, Color.WHITE);
+    private static List<Color> colorList = Arrays.asList(Color.GREEN,Color.BLUE, Color.MAGENTA, Color.CYAN, Color.ORANGE, Color.PINK, Color.YELLOW, Color.WHITE);
+
+    public static Color getRandomColor(){
+
+        Collections.shuffle(colorList);
+
+        return colorList.get(0);
+    }
 
 }
