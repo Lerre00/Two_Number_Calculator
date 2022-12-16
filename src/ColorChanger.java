f716f1c66bad0016e01129072b6ace705e6c70e9
EliasEgel <115075790+EliasEgel@users.noreply.github.com>
2022-12-16 10:48:00 +0100

    List of Colors

---
+++ src/ColorChanger.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ColorChanger {
+    private List<Color> colorList = Arrays.asList(Color.GREEN,Color.BLUE, Color.MAGENTA, Color.CYAN, Color.ORANGE, Color.PINK, Color.YELLOW, Color.WHITE);
+
+}
