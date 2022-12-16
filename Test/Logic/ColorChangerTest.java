e74ed38df3a86f9f06923caf17f772cd7360e3bc
antfou <fougneranton@gmail.com>
2022-12-16 11:57:57 +0100

    added tests to ColorChanger, ColorChanger passed all tests

---
+++ Test/Logic/ColorChangerTest.java
@@ -0,0 +1,20 @@
+package Logic;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class ColorChangerTest {
+    private static List<Color> colorList = Arrays.asList(Color.GREEN,Color.BLUE,
+            Color.MAGENTA, Color.CYAN, Color.ORANGE, Color.PINK, Color.YELLOW, Color.WHITE);
+
+    @Test
+    void getRandomColorTest(){
+
+        assert(colorList.contains(ColorChanger.getRandomColor()));
+        assert(ColorChanger.getRandomColor()!=Color.BLACK);
+
+    }
+}
