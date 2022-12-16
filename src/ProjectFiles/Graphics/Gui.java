333dc9b6806a515e627d332b259bb33346220d60
Jani <Jani.hakala@yh.nackademin.se>
2022-12-16 11:37:09 +0100

    Gui commit

---
+++ src/ProjectFiles/Graphics/Gui.java
@@ -0,0 +1,73 @@
+package ProjectFiles.Graphics;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+
+    private JPanel basePanel = new JPanel();
+    private JPanel northPanel = new JPanel();
+    private JPanel southPanel = new JPanel();
+    private JTextField resultWindow = new JTextField();
+    private JButton button0 = new JButton("0");
+    private JButton button1 = new JButton("1");
+    private JButton button2 = new JButton("2");
+    private JButton button3 = new JButton("3");
+    private JButton button4 = new JButton("4");
+    private JButton button5 = new JButton("5");
+    private JButton button6 = new JButton("6");
+    private JButton button7 = new JButton("7");
+    private JButton button8 = new JButton("8");
+    private JButton button9 = new JButton("9");
+    private JButton commaButton = new JButton(",");
+    private JButton additionButton = new JButton("+");
+    private JButton subtractionButton = new JButton("-");
+    private JButton multiplicationButton = new JButton("*");
+    private JButton divisionButton = new JButton("/");
+    private JButton exponentButton = new JButton("^");
+    private JButton squareRootButton = new JButton("√");
+    private JButton clearButton = new JButton("C");
+    private JButton backspaceButton = new JButton("←");
+    private JButton negativeNumberButton = new JButton("(-)");
+    private JButton swapColorButton = new JButton("☻");
+    private JButton equalsButton = new JButton("=");
+
+    public Gui(){
+        basePanel.setLayout(new BorderLayout());
+        add(basePanel);
+        basePanel.add(northPanel, BorderLayout.NORTH);
+        northPanel.add(swapColorButton);
+        northPanel.add(resultWindow);
+        northPanel.add(backspaceButton);
+        southPanel.setLayout(new GridLayout(5,4));
+        basePanel.add(southPanel, BorderLayout.CENTER);
+
+        southPanel.add(clearButton);
+        southPanel.add(button7);
+        southPanel.add(button4);
+        southPanel.add(button1);
+        southPanel.add(negativeNumberButton);
+
+        southPanel.add(exponentButton);
+        southPanel.add(button8);
+        southPanel.add(button5);
+        southPanel.add(button2);
+        southPanel.add(button0);
+
+        southPanel.add(squareRootButton);
+        southPanel.add(button9);
+        southPanel.add(button6);
+        southPanel.add(button3);
+        southPanel.add(commaButton);
+
+        southPanel.add(divisionButton);
+        southPanel.add(multiplicationButton);
+        southPanel.add(subtractionButton);
+        southPanel.add(additionButton);
+        southPanel.add(equalsButton);
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
